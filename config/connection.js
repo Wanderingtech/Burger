baef994ed838136e0f90367d03457ec82a0ac81f
Heather Stolz <heather86lyn@gmail.com>
2020-10-17 14:55:00 -0700

    DB

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,14 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "vrk7xcrab1wsx4r1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-  port: 3306,
-  user: "xc6wtx7s9efhfmen",
-  password: "w7x85qqy1hrgv1zj",
-  database: "ezv9iai55i0uq46e"
-});
-
+// var connection = mysql.createConnection({
+//   host: "vrk7xcrab1wsx4r1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+//   port: 3306,
+//   user: "xc6wtx7s9efhfmen",
+//   password: "w7x85qqy1hrgv1zj",
+//   database: "ezv9iai55i0uq46e"
+// });
+var connection = mysql.createConnection("mysql://xc6wtx7s9efhfmen:w7x85qqy1hrgv1zj@vrk7xcrab1wsx4r1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ezv9iai55i0uq46e")
 // Make connection.
 connection.connect(function(err) {
   if (err) {
